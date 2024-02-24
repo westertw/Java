eb99102183743f18545e63a4f06ece61ddc6b38d
maksi <hihihaha@mail.ru>
2024-02-24 13:55:34 +0300

    массив 20 чисел

---
+++ src/Basics/Massiv20.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Massiv20 {
+    public static void main(String[] args)
+    {
+        int[] mas = new int[10];
+
+        for (int i = 20; i >= 0; i = i - 2)
+        {
+            System.out.println("Элемент массива: " + i);
+
+        }
+    }
+}
+
+
